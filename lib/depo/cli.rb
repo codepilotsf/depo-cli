dd2cc9570ddb4be04cfe6d4699a8cd600a572446
Sean Schertell <sean@codepilot.com>
2025-05-03 11:16:55 -0700

    Switched to using rbenv to install Ruby – added --verbose option

--- lib/depo/cli.rb
+++ lib/depo/cli.rb
@@ -5,25 +5,26 @@ module Depo
     require 'net/ssh'
 
     def self.start(args)
+      verbose = args.include?('-v') || args.include?('--verbose')
       case args.first
       when 'test'
         puts 'Test successful'
       when 'setup'
-        run_setup
+        run_setup(verbose: verbose)
       else
         puts 'Unknown command'
         exit 1
       end
     end
 
-    def self.run_setup
+    def self.run_setup(verbose: false)
       puts ""
       print green('Hostname or IP address of remote server? ')
       host = STDIN.gets.strip
       user = 'root'
       steps = [
-        {name: 'Installing asdf', cmd: install_asdf_commands},
-        {name: 'Installing Ruby', cmd: install_ruby_commands},
+        {name: 'Installing rbenv', cmd: install_rbenv_commands},
+        {name: 'Installing Ruby', cmd: install_rbenv_ruby_commands},
         {name: 'Installing Puma', cmd: install_puma_commands},
         {name: 'Installing Caddy', cmd: install_caddy_commands},
         {name: 'Starting services', cmd: start_services_commands}
@@ -47,8 +48,8 @@ module Depo
         check_abort(ssh, "command -v asdf", 'asdf')
 
         steps.each do |step|
-          run_step(step[:name], on_success: ->(msg) { green(msg) }, on_fail: ->(msg) { red(msg) }) do
-            run_remote_commands(ssh, step[:cmd])
+          run_step(step[:name], on_success: ->(msg) { green(msg) }, on_fail: ->(msg) { red(msg) }, verbose: verbose) do
+            run_remote_commands(ssh, step[:cmd], verbose: verbose)
           end
         end
         ssh.close
@@ -68,35 +69,53 @@ module Depo
       "\e[31m#{str}\e[0m" # Red
     end
 
-    def self.run_step(name, on_success:, on_fail:)
-      spinner_running = true
-      spinner_thread = Thread.new do
-        spinner = %w[| / - \\]
-        i = 0
-        while spinner_running
-          print "\r#{spinner[i % spinner.length]} #{name}"
-          sleep 0.1
-          i += 1
-        end
-      end
-      begin
-        result = yield
-        spinner_running = false
-        spinner_thread.join
-        if result
-          puts "\r\e[2K\e[1G\e[32m\u2714\e[0m #{on_success.call(name)}"
-          true
-        else
-          puts "\r\e[2K\e[1G" + red("#{on_fail.call(name)} failed")
+    def self.run_step(name, on_success:, on_fail:, verbose: false)
+      if verbose
+        begin
+          result = yield
+          if result
+            puts green("✔ #{on_success.call(name)}")
+            true
+          else
+            puts red("#{on_fail.call(name)} failed")
+            puts
+            exit 1
+          end
+        rescue => e
+          puts red(e.message.to_s)
           puts
           exit 1
         end
-      rescue => e
-        spinner_running = false
-        spinner_thread.join
-        puts "\r\e[2K\e[1G" + red(e.message.to_s)
-        puts
-        exit 1
+      else
+        spinner_running = true
+        spinner_thread = Thread.new do
+          spinner = %w[| / - \\]
+          i = 0
+          while spinner_running
+            print "\r#{spinner[i % spinner.length]} #{name}"
+            sleep 0.1
+            i += 1
+          end
+        end
+        begin
+          result = yield
+          spinner_running = false
+          spinner_thread.join
+          if result
+            puts "\r\e[2K\e[1G\e[32m\u2714\e[0m #{on_success.call(name)}"
+            true
+          else
+            puts "\r\e[2K\e[1G" + red("#{on_fail.call(name)} failed")
+            puts
+            exit 1
+          end
+        rescue => e
+          spinner_running = false
+          spinner_thread.join
+          puts "\r\e[2K\e[1G" + red(e.message.to_s)
+          puts
+          exit 1
+        end
       end
     end
 
@@ -117,11 +136,11 @@ module Depo
       end
     end
 
-    def self.run_remote_commands(ssh, cmds)
-      Array(cmds).all? { |cmd| ssh_exec!(ssh, cmd) }
+    def self.run_remote_commands(ssh, cmds, verbose: false)
+      Array(cmds).all? { |cmd| ssh_exec!(ssh, cmd, verbose: verbose) }
     end
 
-    def self.ssh_exec!(ssh, command)
+    def self.ssh_exec!(ssh, command, verbose: false)
       output = ""
       ssh.open_channel do |channel|
         channel.exec(command) do |_ch, success|
@@ -130,9 +149,11 @@ module Depo
           end
           channel.on_data do |_ch, data|
             output += data
+            puts data if verbose
           end
           channel.on_extended_data do |_ch, _type, data|
             output += data
+            puts data if verbose
           end
         end
       end
@@ -140,29 +161,29 @@ module Depo
       output
     end
 
-    def self.install_asdf_commands
+    def self.install_rbenv_commands
       [
         'apt update',
-        'apt install -y git curl',
-        'git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1',
-        'echo ". $HOME/.asdf/asdf.sh" >> ~/.bashrc',
-        'echo ". $HOME/.asdf/completions/asdf.bash" >> ~/.bashrc',
-        'export PATH="$HOME/.asdf/bin:$PATH"'
+        'apt install -y git curl build-essential libssl-dev libreadline-dev zlib1g-dev libffi-dev libyaml-dev',
+        'git clone https://github.com/rbenv/rbenv.git ~/.rbenv',
+        'echo "export PATH=\"$HOME/.rbenv/bin:$PATH\"" >> ~/.profile',
+        'echo \'eval "$(rbenv init -)"\' >> ~/.profile',
+        'git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build',
+        'export PATH="$HOME/.rbenv/bin:$PATH"',
+        'eval "$(~/.rbenv/bin/rbenv init -)"'
       ]
     end
 
-    def self.install_ruby_commands
+    def self.install_rbenv_ruby_commands
       [
-        '. $HOME/.asdf/asdf.sh',
-        'asdf plugin-add ruby || true',
-        'asdf install ruby latest',
-        'asdf global ruby $(asdf latest ruby)'
+        '~/.rbenv/bin/rbenv install $(~/.rbenv/bin/rbenv install -l | grep -v - | tail -1)',
+        '~/.rbenv/bin/rbenv global $(~/.rbenv/bin/rbenv install -l | grep -v - | tail -1)',
+        '~/.rbenv/bin/rbenv rehash'
       ]
     end
 
     def self.install_puma_commands
       [
-        '. $HOME/.asdf/asdf.sh',
         'gem install puma'
       ]
     end
